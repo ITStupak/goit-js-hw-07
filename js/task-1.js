dd4a139c4a310e3c38f40b87a5c6a4fb1c44788d
Sergey Stupak <itstupak@gmail.com>
2024-05-09 01:56:35 +0200

    finished task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,11 @@
 
 const categories = document.querySelectorAll('li.item');
+const titles = document.querySelectorAll('h2');
+const subList = document.querySelectorAll('li.item > ul');
+
 console.log(`Number of categories: ${categories.length}`);
+for (let i = 0; i < categories.length; i++) {
+    console.log(`Category: ${titles[i].textContent}`);
+    console.log(`Elements: ${subList[i].childElementCount}`);
+};
 
