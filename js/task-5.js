c48ebcc49db070e3477f3f5a6aa9c87173104cd8
Sergey Stupak <itstupak@gmail.com>
2024-05-16 01:49:54 +0200

    finished task-5 hw-07

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColorBtn = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+const changeColor = () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorName.textContent = newColor;
+}
+changeColorBtn.addEventListener('click', changeColor);
